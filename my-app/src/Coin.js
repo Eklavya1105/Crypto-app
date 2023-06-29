877b49ee87fec489c0fd128ca46a53f3761cc44f
Eklavya Agal <eklavyaagal@gmail.com>
2023-06-29 11:45:52 +0530

    crypto

---
+++ my-app/src/Coin.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Coin.css";
+export default function Coin({image,name,price,volume,pricechange,marketcap}){
+    return(
+        <div className="coin-container">
+            <div className="coin-row">
+                <div className="coin">
+                    <img src="{image}" alt="crypto"/>
+                    <h1>{name}</h1>
+                    <p className="coin-symbol"></p>
+                </div>
+                <div className="coin-data">
+                    <p className="coin-price">Rs.{price}</p>
+                    <p className="coin-volume">Rs.{volume.toLocalString()}</p>
+                    {pricechange<0?(
+                        <p className="coin-percent red">{pricechange.toFixed(2)}%</p>
+                    ):(
+                        <p className="coin-percent green">{pricechange.toFixed(2)}%</p>
+                    )
+                    }
+                    <p className="coin-marketcap">
+                        Mkt Cap:Rs.{marketcap.toLocalString()}
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
